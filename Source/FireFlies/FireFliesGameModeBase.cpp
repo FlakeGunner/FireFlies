537a79a6b13599a8fc146b8ab0c9a43e071526fd
Flake Gunner <FlakeGunner@gmail.com>
2017-03-07 16:42:28 +0000

    Base empty UE4 project

---
+++ Source/FireFlies/FireFliesGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FireFlies.h"
+#include "FireFliesGameModeBase.h"
+
+
+
+
---
+++ Source/FireFlies/FireFliesGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FireFliesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIREFLIES_API AFireFliesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
